1ea00102d94b1f51b0c92ee93671a6727637cd8b
Hullmano <hullmano@gmail.com>
2020-05-20 17:18:39 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;               //- para definir rotas.
@@ -21,6 +23,8 @@ $app->get('/', function() {   //aqui são definidas as rotas. Neste caso "/" é 
 
 $app->get('/admin', function() {   //aqui são definidas as rotas. Neste caso "/" é a raiz.
 
+	User::verfifylogin();          //verifica se há uma sessão e se o user é admin.
+
 	$page = new PageAdmin();       //chama a classe Page. Esta classe posso o construct e o destruct, que são executados assim que instanciada a classe. Dentro do construct chama o header, e no destruct chama o footer.
 
 	$page->setTpl("index");   //chama o método que tras o body html.
@@ -40,8 +44,19 @@ $app->get('/admin/login', function() {   //aqui são definidas as rotas. Neste c
 
 $app->post('/admin/login', function() {
 
-	User::login($_POST["login"], $_POST["password"]);
+	User::login($_POST["login"], $_POST["password"]);  //recebe o valores via post. User::login - métod.static para validar login/senha
 
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();                  //aqui chama as rotas.
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -3,10 +3,13 @@
 namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
+use \Hcode\Model;
 
-class User {
+class User extends Model{
 
-	public static function login($login, $password = "1")
+	const SESSION = "User";   //nome da sessão.
+
+	public static function login($login, $password)
 	{
 
 		$sql = new Sql();
@@ -23,14 +26,51 @@ class User {
 
 		$data = $results[0];
 
+		//print_r($data);
+
 		if (password_verify($password, $data["despassword"]) === true)         //- faz um verific. e retorna um boolean.
 		{
 			$user = new User();
+
+			#$user->setiduser($data["iduser"]);
+			#$user->setidperson($data["idperson"]);
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();  //iniciando uma sessão e passando como dados, os mesmos $data;
+
+			return $user;
 			
 		} else {
 			throw new \Exception("Usuário ou Senha Inválido.");
 		}
 
+
+	}#fim function login.
+
+	public static function verfifylogin($inadmin = true)
+	{
+
+		if (
+			!isset($_SESSION[User::SESSION])                //verifica se a sessão foi criada.
+			||
+			!$_SESSION[User::SESSION]                       //verifica se está vazia.
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0   //verifica se existe um id na sessão.
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin //verifica se é admin.
+		) {
+			header("Location: /admin/login");
+			exit;
+
+		}
+
+	}#fim function verfifylogin
+
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
 
 	}
 
